a1f99e6712fc6304428514223460127c6171ff13
jaycohen88 <jaycohen88@gmail.com>
2020-04-26 02:55:24 -0400

    full code resulting in transpose of data

--- MLS_scrape.py
+++ MLS_scrape.py
@@ -23,35 +23,35 @@ teams_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keepe
 teams_2019 = []
 for team19 in teams_2019_elements:
     print(team19.text)
-	teams_2019.append(team19.text)
+    teams_2019.append(team19.text)
 
 # Scrape matches played
 matches_played_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[2]')
 matches_played_2019 = []
 for matches19 in matches_played_2019_elements:
     print(matches19.text)
-	matches_played_2019.append(matches19.text)
+    matches_played_2019.append(matches19.text)
 
 # Scrape wins
 wins_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[10]')
 wins_2019 = []
 for win19 in wins_2019_elements:
     print(win19.text)
-	wins_2019.append(win19.text)
+    wins_2019.append(win19.text)
 
 # Scrape draws
 draws_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[11]')
 draws_2019 = []
 for draw19 in draws_2019_elements:
     print(draw19.text)
-	draws_2019.append(draw19.text)
+    draws_2019.append(draw19.text)
 
 # Scrape losses
 losses_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[12]')
 losses_2019 = []
 for loss19 in losses_2019_elements:
     print(loss19.text)
-	losses_2019.append(loss19.text)
+    losses_2019.append(loss19.text)
 
 # Scroll back to top of page for easier in-page navigation
 driver.execute_script('window.scrollTo(0, 0)')
@@ -67,7 +67,7 @@ completed_crosses_into_box_2019_elements = driver.find_elements_by_xpath('.//div
 completed_crosses_into_box_2019 = []
 for compcross19 in completed_crosses_into_box_2019_elements:
     print(compcross19.text)
-	completed_crosses_into_box_2019.append(compcross19.text)
+    completed_crosses_into_box_2019.append(compcross19.text)
 
 # Scroll back to top of page for easier in-page navigation
 driver.execute_script('window.scrollTo(0, 0)')
@@ -83,7 +83,7 @@ crosses_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_pas
 crosses_2019 = []
 for cross19 in crosses_2019_elements:
     print(cross19.text)
-	crosses_2019.append(cross19.text)
+    crosses_2019.append(cross19.text)
 
 # Scroll back to top of page for easier in-page navigation
 driver.execute_script('window.scrollTo(0, 0)')
@@ -99,21 +99,21 @@ pressures_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_d
 pressures_2019 = []
 for press19 in pressures_2019_elements:
     print(press19.text)
-	pressures_2019.append(press19.text)
+    pressures_2019.append(press19.text)
 
 # Scrape successful pressures
 successful_pressures_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_defense_squads"]/table/tbody/tr/td[12]')
 successful_pressures_2019 = []
 for sp19 in successful_pressures_2019_elements:
     print(sp19.text)
-	successful_pressures_2019.append(sp19.text)
+    successful_pressures_2019.append(sp19.text)
 
 # Scrape number of pressures in the attacking third
 num_high_press_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_defense_squads"]/table/tbody/tr/td[16]')
 num_high_press_2019 = []
 for hp19 in num_high_press_2019_elements:
     print(hp19.text)
-	num_high_press_2019.append(hp19.text)
+    num_high_press_2019.append(hp19.text)
 
 # Scroll back to top of page for easier in-page navigation
 driver.execute_script('window.scrollTo(0, 0)')
@@ -129,28 +129,28 @@ goals_scored_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stat
 goals_scored_2019 = []
 for goal19 in goals_scored_2019_elements:
     print(goal19.text)
-	goals_scored_2019.append(goal19.text)
+    goals_scored_2019.append(goal19.text)
 
 # Scrape goal difference
 goal_difference_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[14]')
 goal_difference_2019 = []
 for diff19 in goal_difference_2019_elements:
     print(diff19.text)
-	goal_difference_2019.append(diff19.text)
+    goal_difference_2019.append(diff19.text)
 
 # Scrape expected goals scored
 expected_goals_scored_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[16]')
 expected_goals_scored_2019 = []
 for xgs19 in expected_goals_scored_2019_elements:
     print(xgs19.text)
-	expected_goals_scored_2019.append(xgs19.text)
+    expected_goals_scored_2019.append(xgs19.text)
 
 # Scrape expected goal differential
 expected_goal_difference_2019_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[18]')
 expected_goal_difference_2019 = []
 for xgd19 in expected_goal_difference_2019_elements:
     print(xgd19.text)
-	expected_goal_difference_2019.append(xgd19.text)
+    expected_goal_difference_2019.append(xgd19.text)
 
 # Scroll back to top of page to go to prior year's data
 driver.execute_script('window.scrollTo(0, 0)')
@@ -165,31 +165,31 @@ teams_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keepe
 teams_2018 = []
 for team18 in teams_2018_elements:
     print(team18.text)
-	teams_2018.append(team18.text)
+    teams_2018.append(team18.text)
 
 matches_played_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[2]')
 matches_played_2018 = []
 for matches18 in matches_played_2018_elements:
     print(matches18.text)
-	matches_played_2018.append(matches18.text)
+    matches_played_2018.append(matches18.text)
 
 wins_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[10]')
 wins_2018 = []
 for win18 in wins_2018_elements:
     print(win18.text)
-	wins_2018.append(win18.text)
+    wins_2018.append(win18.text)
 
 draws_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[11]')
 draws_2018 = []
 for draw18 in draws_2018_elements:
     print(draw18.text)
-	draws_2018.append(draw18.text)
+    draws_2018.append(draw18.text)
 
 losses_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_keeper_squads"]/table/tbody/tr/td[12]')
 losses_2018 = []
 for loss18 in losses_2018_elements:
     print(loss18.text)
-	losses_2018.append(loss18.text)
+    losses_2018.append(loss18.text)
 
 driver.execute_script('window.scrollTo(0, 0)')
 
@@ -200,7 +200,7 @@ completed_crosses_into_box_2018_elements = driver.find_elements_by_xpath('.//div
 completed_crosses_into_box_2018 = []
 for compcross18 in completed_crosses_into_box_2018_elements:
     print(compcross18.text)
-	completed_crosses_into_box_2018.append(compcross18.text)
+    completed_crosses_into_box_2018.append(compcross18.text)
 
 driver.execute_script('window.scrollTo(0, 0)')
 
@@ -211,7 +211,7 @@ crosses_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_pas
 crosses_2018 = []
 for cross18 in crosses_2018_elements:
     print(cross18.text)
-	crosses_2018.append(cross18.text)
+    crosses_2018.append(cross18.text)
 
 driver.execute_script('window.scrollTo(0, 0)')
 
@@ -222,19 +222,19 @@ pressures_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_d
 pressures_2018 = []
 for press18 in pressures_2018_elements:
     print(press18.text)
-	pressures_2018.append(press18.text)
+    pressures_2018.append(press18.text)
 
 successful_pressures_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_defense_squads"]/table/tbody/tr/td[12]')
 successful_pressures_2018 = []
 for sp18 in successful_pressures_2018_elements:
     print(sp18.text)
-	successful_pressures_2018.append(sp18.text)
+    successful_pressures_2018.append(sp18.text)
 
 num_high_press_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_defense_squads"]/table/tbody/tr/td[16]')
 num_high_press_2018 = []
 for hp18 in num_high_press_2018_elements:
     print(hp18.text)
-	num_high_press_2018.append(hp18.text)
+    num_high_press_2018.append(hp18.text)
 
 driver.execute_script('window.scrollTo(0, 0)')
 
@@ -245,35 +245,35 @@ goals_scored_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stat
 goals_scored_2018 = []
 for goal18 in goals_scored_2018_elements:
     print(goal18.text)
-	goals_scored_2018.append(goal18.text)
+    goals_scored_2018.append(goal18.text)
 
 goal_difference_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[14]')
 goal_difference_2018 = []
 for diff18 in goal_difference_2018_elements:
     print(diff18.text)
-	goal_difference_2018.append(diff18.text)
+    goal_difference_2018.append(diff18.text)
 
 expected_goals_scored_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[16]')
 expected_goals_scored_2018 = []
 for xgs18 in expected_goals_scored_2018_elements:
     print(xgs18.text)
-	expected_goals_scored_2018.append(xgs18.text)
+    expected_goals_scored_2018.append(xgs18.text)
 
 expected_goal_difference_2018_elements = driver.find_elements_by_xpath('.//div[@id="div_stats_playing_time_squads"]/table/tbody/tr/td[18]')
 expected_goal_difference_2018 = []
 for xgd18 in expected_goal_difference_2018_elements:
     print(xgd18.text)
-	expected_goal_difference_2018.append(xgd18.text)
+    expected_goal_difference_2018.append(xgd18.text)
 
-#Write to csv file
-rows = [teams_2019, matches_played_2019, wins_2019, draws_2019, losses_2019, goals_scored_2019, goal_difference_2019, \
+# Write to csv file
+rows = (teams_2019, matches_played_2019, wins_2019, draws_2019, losses_2019, goals_scored_2019, goal_difference_2019, \
 expected_goals_scored_2019, expected_goal_difference_2019, crosses_2019, completed_crosses_into_box_2019, pressures_2019, \
 successful_pressures_2019, num_high_press_2019, teams_2018, matches_played_2018, wins_2018, draws_2018, losses_2018, goals_scored_2018, \
 goal_difference_2018, expected_goals_scored_2018, expected_goal_difference_2018, crosses_2018, completed_crosses_into_box_2018, \
-pressures_2018, successful_pressures_2018, num_high_press_2018]
+pressures_2018, successful_pressures_2018, num_high_press_2018)
 
-writer.writerow(rows)
+for row in rows:
+    writer.writerow(row)
 
-# Close csv file
 csv_file.close()
 driver.close()
